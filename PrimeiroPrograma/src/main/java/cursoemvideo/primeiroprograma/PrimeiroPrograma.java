617b1b8afc148dc7a9dd5ae66ad55fe50a00517e
André L Josefino <ALJosefino@gmail.com>
2023-10-31 14:14:48 -0300

    Atalhos sout e psvm
    Comandos básico sout e psvm usando o NetBeans 13 e as diferenças print e printLN

--- PrimeiroPrograma/src/main/java/cursoemvideo/primeiroprograma/PrimeiroPrograma.java
+++ PrimeiroPrograma/src/main/java/cursoemvideo/primeiroprograma/PrimeiroPrograma.java
@@ -7,6 +7,11 @@ public class PrimeiroPrograma {
         System.out.print("Holla Mundo");
         System.out.print("Olá, Mundo!");
         System.out.println("Olá, Mundo!");
+        System.out.println("sout mais tecla tab vai escrever o comando System.out.println");
+        System.out.println("psm mais tecla tab vai escrever public static void main");
+        
+        
+        
         
     }
 }
