67e2bc40a1a1d8e2d2b81c52ff13a830845c4521
André L Josefino <ALJosefino@gmail.com>
2023-10-31 14:06:44 -0300

    Movida a pasta PrimeiroPrograma
    O projeto PrimeiroPrograma da Aula 4  movido para o repositorio publicado

---
+++ PrimeiroPrograma/src/main/java/cursoemvideo/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package cursoemvideo.primeiroprograma;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        System.out.print("Holla Mundo");
+        System.out.print("Olá, Mundo!");
+        System.out.println("Olá, Mundo!");
+        
+    }
+}
