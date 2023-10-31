8534f9f652da256b63c487263dfd33c374c5c542
André L Josefino <ALJosefino@gmail.com>
2023-10-31 15:22:50 -0300

    Atalhos do NetBeans13 Basico
    Atalhos de psvm para inserir métodos e a classe System sout

--- PrimeiroPrograma/src/main/java/cursoemvideo/primeiroprograma/PrimeiroPrograma.java
+++ PrimeiroPrograma/src/main/java/cursoemvideo/primeiroprograma/PrimeiroPrograma.java
@@ -8,7 +8,7 @@ public class PrimeiroPrograma {
         System.out.print("Olá, Mundo!");
         System.out.println("Olá, Mundo!");
         System.out.println("sout mais tecla tab vai escrever o comando System.out.println");
-        System.out.println("psm mais tecla tab vai escrever public static void main");
+        System.out.println("psvm mais tecla tab vai escrever public static void main");
         
         
         
