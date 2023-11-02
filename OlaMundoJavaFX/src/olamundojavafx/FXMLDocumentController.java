6cf6423e042922e232228f22a952746510871fe8
André L Josefino <ALJosefino@gmail.com>
2023-11-02 11:15:40 -0300

    Ola Mundo JavaFX
    Adicionado botão e mensagem ola mundo com JavaFX

--- OlaMundoJavaFX/src/olamundojavafx/FXMLDocumentController.java
+++ OlaMundoJavaFX/src/olamundojavafx/FXMLDocumentController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
- */
 package olamundojavafx;
 
 import java.net.URL;
@@ -9,21 +5,18 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-/**
- *
- * @author andre
- */
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private Label lblMensagem;
+    private Button btnClick;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void clicouBotao(ActionEvent event) {
+        lblMensagem.setText("Olá, Mundo!");
     }
     
     @Override
