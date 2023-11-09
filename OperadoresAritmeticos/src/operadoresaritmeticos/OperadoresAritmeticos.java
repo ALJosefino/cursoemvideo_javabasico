42b9326efe224414125025b7252294b226d73e84
André L Josefino <ALJosefino@gmail.com>
2023-11-08 23:09:46 -0300

    Operadores Aritmeticos Precedência
    Exemplo com divisião primeiro e depois soma não considerada como media.

--- OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -7,8 +7,15 @@ public class OperadoresAritmeticos {
     public static void main(String[] args) {
       int n1 = 3;
       int n2 = 5;
-      float media = (n1 + n2)/2;
+      float media = (n1+n2)/2;
       System.out.println("A média é igual a " + media);
+      
+      int n3 = 6;
+      int n4 = 4;
+      float media1 = n3+n4/2;
+      System.out.println("A regra da precedência começa primeiro a \n"
+              + "divisão e depois soma " + media1);
+      
     }
     
 }
