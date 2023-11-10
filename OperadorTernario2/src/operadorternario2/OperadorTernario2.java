72f6d5a67ddbb9809024242b773babb1c3202f26
André L Josefino <ALJosefino@gmail.com>
2023-11-10 20:19:42 -0300

    Operador Ternário
    Testes com operadores ternários usando maior ou menor e igual.

--- OperadorTernario2/src/operadorternario2/OperadorTernario2.java
+++ OperadorTernario2/src/operadorternario2/OperadorTernario2.java
@@ -4,6 +4,9 @@ public class OperadorTernario2 {
 
     public static void main(String[] args) {
      
+        System.out.println("Operador ternário: 0 se verdadeiro");
+        System.out.println("Operador ternário: 1 se falso");
+        
         int n1 = 4, n2 = 3;
         
         int r = n1 > n2 ? 0 : 1;
@@ -16,6 +19,12 @@ public class OperadorTernario2 {
         x = k1 > k2 ? 0 : 1;
         
         System.out.printf("Resultado de k1 < k2: %d \n", x);
+        
+        int f1 = 7, f2 = 7, d;
+        
+        d = f1 == f2 ? 0 : 1;
+        
+        System.out.printf("Resultado de f1 = f2: %d \n", d);
         
         
     }
