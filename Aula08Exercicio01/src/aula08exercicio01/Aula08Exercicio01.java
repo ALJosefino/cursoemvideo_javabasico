08e6e7f8758afadd8f3aff50ae8c0f5fa1888cbe
André L Josefino <ALJosefino@gmail.com>
2023-11-11 09:33:30 -0300

    Testando operador ternário
    Teste de operador ternário com tipo de resultado que tipo primitivo Integer retorna mesmo com conteúdo em número real.

---
+++ Aula08Exercicio01/src/aula08exercicio01/Aula08Exercicio01.java
@@ -0,0 +1,15 @@
+package aula08exercicio01;
+
+public class Aula08Exercicio01 {
+
+    public static void main(String[] args) {
+        int x = 8, y = 3;
+        
+        int w =  x / y;
+        
+        String z = (w % 2 == 0)?"frase1":"frase2";
+        
+        System.out.println(z);
+    }
+    
+}
