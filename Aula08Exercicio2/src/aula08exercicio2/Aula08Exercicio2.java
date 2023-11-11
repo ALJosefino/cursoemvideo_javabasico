5a82ebc5085ff895511ae0e6ee52b909ae97e782
André L Josefino <ALJosefino@gmail.com>
2023-11-11 09:43:41 -0300

    Comparando Strings
    Testando diferença entre objetos e conteúdo de objetos da classe String.

---
+++ Aula08Exercicio2/src/aula08exercicio2/Aula08Exercicio2.java
@@ -0,0 +1,20 @@
+package aula08exercicio2;
+
+public class Aula08Exercicio2 {
+    public static void main(String[] args) {
+        
+        String parte1 = "Curso";
+        
+        String parte2 = "Video";
+        
+        String parte3 = parte1 + parte2;
+        
+        String parte4 = "CursoVideo";
+        
+        System.out.println(parte3 == parte4);
+        
+        System.out.println(parte3.equals(parte4));
+
+    }
+    
+}
