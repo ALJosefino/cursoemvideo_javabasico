c8669202e5a7b999689fa63972e5a2d6ff0528fd
André L Josefino <ALJosefino@gmail.com>
2023-11-13 18:21:28 -0300

    Aula10 Exercicio 02
    Criação da Classe Teste como principal e passagem de conteúdo por método  que usa teste se varável esta vazia e imprime conteúdo que veio de outra

---
+++ 10Aula/Aula10Exercicio02/src/Aula10Exercicio02/Teste.java
@@ -0,0 +1,29 @@
+
+package Aula10Exercicio02;
+
+/**
+ *
+ * @author andre
+ */
+
+public class Teste {
+    
+    public static void main(String [] args) {
+        
+        String nome = "João";
+        
+        imprimeNome("Empty");
+    }
+    
+    public static void imprimeNome(String nome) {
+        
+        if (!nome.isEmpty()) {
+        
+            System.out.println("Tudo bem " + nome + "?");
+        
+        } else {
+                
+                System.out.println("O nome é " + nome + "?");
+        }
+    }
+}
