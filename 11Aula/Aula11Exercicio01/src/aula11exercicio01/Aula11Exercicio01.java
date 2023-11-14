725f29c45bc6711d63231e8957950a34a0a1321e
André L Josefino <ALJosefino@gmail.com>
2023-11-14 15:58:31 -0300

    Aula 11 Exercício 1
    Testando tipos primitivos char, int long e ainda um objeto da Classe String

---
+++ 11Aula/Aula11Exercicio01/src/aula11exercicio01/Aula11Exercicio01.java
@@ -0,0 +1,38 @@
+
+package aula11exercicio01;
+
+/**
+ *
+ * @author andre
+ */
+public class Aula11Exercicio01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        char c = 'c';
+        
+        int i = 10;
+        
+        double d = 10;
+        
+        long l = 1;
+        
+        String s = "Hello";
+        
+        // ERRADA A c = c + i;
+        
+        // CORRETA B s += i;
+        
+        // ERRADA C i += s;
+        
+        // ERRADA D c += s;
+        
+        // CORRETA E i += l;
+        
+        
+    }
+    
+}
