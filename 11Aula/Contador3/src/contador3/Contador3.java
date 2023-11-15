394dcc8d4bc9d47ac638c26569a4d1d7a323dee5
André L Josefino <ALJosefino@gmail.com>
2023-11-15 13:13:03 -0300

    Contador 3
    Objeto teclado instaciado da Classe Scanner recebe um  inteiro que calcula o fatorial , usa Estrutura de repetição  Enquanto que testa se o número esta entre menor do que 15 ou menor do que 0, depois outro método while que pré decrementa calculando com operador de atribuição o fatorial.

---
+++ 11Aula/Contador3/src/contador3/Contador3.java
@@ -0,0 +1,54 @@
+
+package contador3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author andre
+ */
+
+public class Contador3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+    
+            System.out.print("De qual número de 1 até 15 você quer saber o "
+                    + "fatorial? Por favor digite:");
+            
+            int n = teclado.nextInt();
+            
+                       
+            System.out.printf("O número digitado foi %d \n", n);
+            
+            while ( n >= 16 || n < 0 ) {
+                
+                System.out.printf("Número %d acima de 15 ou inferior a 0 " +
+                        "por favor digite um número entre 0 e 15: ", n);
+                        
+                        n = teclado.nextInt();
+                                
+                }
+            
+            int c = n;
+            
+            int f = 1;
+            
+            while (c >= 1) {
+            
+                f *= c;
+                
+                c --;
+                
+            }
+            
+            System.out.printf("Fatorial do número %d é o número %d ", n, f);
+                  
+        }
+    
+}
