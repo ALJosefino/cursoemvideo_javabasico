1ab2d6321a308a699a7e4a8b648a1fe4bc1abea1
André L Josefino <ALJosefino@gmail.com>
2023-11-16 15:09:04 -0300

    Repetição com Variável de Controle
    Comando para com exemplo  de contagens  e sintaxe, usado operadores de atribuição e pós incremento

---
+++ 13Aula/RepeticaoPara/src/repeticaopara/RepeticaoPara.java
@@ -0,0 +1,41 @@
+
+package repeticaopara;
+
+/**
+ *
+ * @author andre
+ */
+
+public class RepeticaoPara {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        
+        int cc;
+        
+        for ( cc=0; cc < 4; cc++ ) {
+        
+            System.out.println("Cambalhota" + cc);
+            
+        }
+        
+        int dd;
+        
+        for (dd = 0; dd <= 4; dd++ ) {
+        
+            System.out.println("Pulo" + dd);
+                    
+        }
+        
+        int xx;
+        
+        for (xx = 0; xx <= 100; xx +=10 ) {
+        
+            System.out.println("Salto" + xx);
+        }
+    }
+    
+}
