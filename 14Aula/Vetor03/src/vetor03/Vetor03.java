cb57996cbabe9984475204e20a142c5140e78df6
André L Josefino <ALJosefino@gmail.com>
2023-11-18 11:30:46 -0300

    Demonstração de vetores
    Declarado dois vetores um instaciado e atribuido em conteúdo inteiro em cada indice. Instaciado vetor com atribuições direta na criação, ambos vetores mostrado os conteúdos.

---
+++ 14Aula/Vetor03/src/vetor03/Vetor03.java
@@ -0,0 +1,40 @@
+
+package vetor03;
+
+/**
+ *
+ * @author andre
+ */
+
+public class Vetor03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        
+        int n[] = new int[4];
+                
+        n[0] = 3;
+        n[1] = 5;
+        n[2] = 8;
+        n[3] = 2;
+        
+        System.out.println(n[0]);
+        System.out.println(n[1]);
+        System.out.println(n[2]);
+        System.out.println(n[3]);
+        
+        
+        int []x = {9,1,7,0,6};
+        
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+        System.out.println(x[2]);
+        System.out.println(x[3]);
+        
+        
+    }
+    
+}
