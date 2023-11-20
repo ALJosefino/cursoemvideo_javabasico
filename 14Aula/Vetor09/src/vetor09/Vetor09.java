fbba4c0afc31215d1ef22246489f9baef49a132a
André L Josefino <ALJosefino@gmail.com>
2023-11-20 20:37:33 -0300

    Vetor simples com atribuição
    declarado vetor com atribuição direta de conteúdo tipo primitivo inteiro, impresso cada conteúdo com formato inteiro.

---
+++ 14Aula/Vetor09/src/vetor09/Vetor09.java
@@ -0,0 +1,28 @@
+
+package vetor09;
+
+/**
+ *
+ * @author andre
+ */
+public class Vetor09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int n[] = {3,5,8,2};
+        
+        System.out.format("%d\n", n[0]);
+        System.out.format("%d\n", n[1]);
+        System.out.format("%d\n", n[2]);
+        System.out.format("%d\n", n[3]);
+        
+        
+        
+        
+        
+    }
+    
+}
