39f1723fa2a8178014f8676052c0f97c24e141e3
André L Josefino <ALJosefino@gmail.com>
2023-11-20 20:32:19 -0300

    vetor simples
    vetor instaciado e atribuidos aos indices manualmente os conteúdos em tipo primitivos em inteiro, impressos na tela

---
+++ 14Aula/Vetor08/src/vetor08/Vetor08.java
@@ -0,0 +1,28 @@
+package vetor08;
+
+/**
+ *
+ * @author andre
+ */
+public class Vetor08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n[] = new int [4];
+        
+        n[0] = 3;
+        n[1] = 5;
+        n[2] = 8;
+        n[3] = 2;
+        
+        System.out.print(n[0]);
+        System.out.print(n[1]);
+        System.out.print(n[2]);
+        System.out.print(n[3]);
+                
+                
+    }
+    
+}
