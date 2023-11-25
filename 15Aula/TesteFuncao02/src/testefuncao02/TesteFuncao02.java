13779b1d49a76440ab1e9615b343461d0acb6396
André L Josefino <ALJosefino@gmail.com>
2023-11-25 01:14:08 -0300

    Funções e Classes
    Criadas duas classes a primeira classe invoca a segunda onde mostra uma contagem e colocada em visibilidade pública e retrona o resultado de um laço para

---
+++ 15Aula/TesteFuncao02/src/testefuncao02/TesteFuncao02.java
@@ -0,0 +1,23 @@
+
+package testefuncao02;
+
+/**
+ *
+ * @author andre
+ */
+
+public class TesteFuncao02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+                
+        System.out.println("Vai começar a contagem");
+        
+        System.out.println(Operacoes.contador(1,5));
+        
+    }
+    
+}
