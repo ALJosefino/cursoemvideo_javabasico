1f01715e350a76d64fdb235a8600bc1a0dd6bf5f
André L Josefino <ALJosefino@gmail.com>
2024-01-23 20:48:00 -0300

    .

---
+++ 15Aula/Teste/src/teste/Teste.java
@@ -0,0 +1,16 @@
+
+package teste;
+
+public class Teste {
+
+    public static void main(String[] args) {
+       
+        double x =Math.pow(8,2);
+        
+        System.out.println(x);
+        
+        
+        
+    }
+    
+}
